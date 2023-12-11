1cc56e252330d88823e02ee599b4f0a9468e871a
james hedley <james.hedley@uk.ibm.com>
2023-12-11 23:37:23 +0000

    day 10 part 2 grrrr

--- 2023/day10.py
+++ 2023/day10.py
@@ -10,8 +10,6 @@ def find_start(l0):
 def it(l0, xx,yy, pipes, dirs):
     OPP = {"N" :"S", "E":"W", "S":"N", "W":"E"}
     cur = l0[yy][xx]
-    #if cur == ".":
-    #    return []
 
     found = []
     for dir, vec in dirs.items():
@@ -25,19 +23,13 @@ def it(l0, xx,yy, pipes, dirs):
         adj = l0[ny][nx]
         if adj in (".", "S"):
             continue
-        #print(adj)
         print("%s %s" % (cur, adj))
         if cur == "S":
             if OPP[dir] in pipes[adj]:
-                #found.append((ny, nx))
                 found.append((nx, ny))
         else:
-            try:
-                if OPP[dir] in pipes[adj] and dir in pipes[cur]:
-                    #found.append((ny, nx))
-                    found.append((nx, ny))
-            except KeyError:
-                print("HEY")
+            if OPP[dir] in pipes[adj] and dir in pipes[cur]:
+                found.append((nx, ny))
         
     return found
 
@@ -65,20 +57,16 @@ def pt1(l0):
     frontier = set(it(l0, startx, starty, pipes, dirs))
 
     while frontier:
-        print("###########")
         fx, fy = frontier.pop()
         found = set(it(l0, fx, fy, pipes, dirs))
-        for fffx, fffy in found:
-            if l0[fffy][fffx] == ".":
-                print("ffffffffff")
         frontier = frontier.union(found)
         frontier = frontier.difference(path)
         path.add((fx,fy))
-        print(frontier)
+        #print(frontier)
     
     print("########")
     print(path)
-    ans = int(len(path)/2)
+    ans = int(len(path)/2) # the shortest route is just half the path length!!
     print(ans)
     return ans
 
@@ -103,11 +91,80 @@ def test():
     ans = pt1(l0)
     assert ans == 8
 
-def main():
+def pt1_main():
     with open("day10_data.txt") as h0:
         l0 = [list(x.strip()) for x in h0.readlines()]
     ans = pt1(l0)
     print(ans)
 
-test()
-main()
+def pt2():
+    #with open("day10_pt2_ex0.txt") as h0:
+    #with open("day10_pt2_ex1.txt") as h0:
+    with open("day10_pt2_ex2.txt") as h0:
+        l0 = [list(x.strip()) for x in h0.readlines()]
+
+    candi = set()
+    for y, row in enumerate(l0):
+        print("".join(row))
+        for x, char in enumerate(row):
+            if char == ".":
+                candi.add((x,y))
+    print(sorted(sorted(list(candi), key= lambda x: x[1]), key= lambda x: x[0]))
+
+    inside = set()
+    for x,y in candi:
+        print((x,y))
+        #look left
+        l_col = l0[y][0:x]
+
+        #look right
+        r_col = l0[y][x+1:len(l0[y])]
+        print(r_col)
+        #look up
+        up_col = []
+        for _yy, row in enumerate(l0[:y+1]):
+            up_col.append(row[x])
+        #look down
+        down_col = []
+        for _yy, row in enumerate(l0[y+1:]):
+            down_col.append(row[x])
+        if (x,y) == (3,2):
+            print("break")
+
+        if (("|" in l_col or "F" in l_col or "L" in l_col)
+         and ("|" in r_col or "7" in r_col or "J" in r_col)
+         and ("-" in up_col or "F" in up_col or "7" in up_col)
+         and ("-" in down_col or "L" in down_col or "J" in down_col)):
+            inside.add((x,y))
+
+    print(inside)
+    print(len(inside))
+    for y, row in enumerate(l0):
+        for x, char in enumerate(row):
+            if (x,y) in inside:
+                #l0[y][x] = "I"
+                l0[y][x] = "*"
+
+    for y, row in enumerate(l0):
+        print("".join(row))
+
+    dirs={}
+    dirs["N"] = (0,-1)
+    dirs["E"] = (1,0)
+    dirs["S"] = (0,1)
+    dirs["W"] = (-1,0)
+    
+    remove = set()
+    for x,y in inside:
+        for _, vec in dirs.items():
+            nx = x+vec[0]
+            ny = y+vec[1]
+            if l0[ny][nx] == ".":
+                remove.add((x,y))
+    inside -= remove
+    print(inside)
+    print(len(inside))
+
+#test()
+#pt1_main()
+pt2()
