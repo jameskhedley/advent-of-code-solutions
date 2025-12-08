c756a8dc7b46317c55e4d0cc2401bae3a9c3928a
james hedley <james.hedley@uk.ibm.com>
2025-12-08 22:25:02 +0000

    fixed ignores

--- 2025/day8.py
+++ 2025/day8.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+# quite slow but ran out of time to optimise it much
 from itertools import chain
 import math
 ex=False
@@ -9,11 +9,6 @@ def get_data():
         h0 = open("./day8.txt")
     l0 = [tuple([int(x) for x in n.strip().split(',')]) for n in h0.readlines()]
     return l0
-
-
-ex = True
-data = get_data()
-#print(data)
 
 def calc_distances(data):
     dist = {}
@@ -31,67 +26,53 @@ def part1(data):
     dist = calc_distances(data)
     circuits = []
     cables = 10 if ex else 1000
+    unconnected = set(data)
     for x in range(cables):
         if x%10 == 0: print(x)
-        closest = list(dist.items())[0][0]
-        del dist[closest]
-        first = sorted(closest,key=lambda x: x[0])[0]
-        second = sorted(closest,key=lambda x: x[0])[1]
-        found = [] # ((906, 360, 560), (984, 92, 344)) is in two circuits!
-        for cc in circuits:
-            if first in cc and second in cc:
-                found.append(cc)
-            elif first in cc and not second in cc:
-                cc.append(second)
-                found.append(cc)
-            elif first not in cc and second in cc:
-                cc.append(first)
-                found.append(cc)
-            if len(found) > 1: #merge shared circuits
-                ncc = list(set(chain(*found)))
-                for fl in found:
-                    circuits.remove(fl)
-                circuits.append(ncc)
-                break
-        if not found:
-            circuits.append([first,second])
+        dist, circuits, first, second, unconnected = connectify(dist, circuits, unconnected)
     return math.prod([len(x) for x in sorted(circuits, key=lambda y: len(y), reverse=True)][:3])
 
 def part2(data):
     dist = calc_distances(data)
     circuits = []
     cables = 0
-    while dist:
-    #while len(circuits) > 1 or cables < 10:
+    unconnected = set(data)
+    while unconnected:
         cables += 1
         if cables % 10 == 0: print(cables)
-        #if len(dist) == 2:
-        #if len(circuits) == 1 and cables > 10:
-        if cables == 23:
-            print("break")
-        closest = list(dist.items())[0][0]
-        first, second = sorted(closest,key=lambda x: x[0])
-        del dist[closest]
-        found = [] # ((906, 360, 560), (984, 92, 344)) is in two circuits!
-        for cc in circuits:
-            oldcc = cc[:]
-            if first in cc and second in cc:
-                found.append(cc)
-            elif first in cc and not second in cc:
-                cc.append(second)
-                found.append(cc)
-            elif first not in cc and second in cc:
-                cc.append(first)
-                found.append(cc)
-            if len(found) > 1: #merge shared circuits
-                ncc = list(set(chain(*found)))
-                for fl in found:
-                    circuits.remove(fl)
-                circuits.append(ncc)
-                break
-        if not found:
-            circuits.append([first,second])
-    print(circuits)
+        dist, circuits, first, second, unconnected = connectify(dist, circuits, unconnected)
+    return first[0] * second[0]
+
+def connectify(dist, circuits, unconnected):
+    closest = list(dist.items())[0][0]
+    first, second = sorted(closest,key=lambda x: x[0])
+    del dist[closest]
+    found = [] # ((906, 360, 560), (984, 92, 344)) is in two circuits!
+    for cc in circuits:
+        if first in cc and second in cc:
+            found.append(cc)
+        elif first in cc and not second in cc:
+            cc.append(second)
+            found.append(cc)
+        elif first not in cc and second in cc:
+            cc.append(first)
+            found.append(cc)
+        if len(found) > 1: #merge shared circuits
+            ncc = list(set(chain(*found)))
+            for fl in found:
+                circuits.remove(fl)
+            circuits.append(ncc)
+            break
+    if not found:
+        circuits.append([first,second])
+    if first in unconnected:
+        unconnected.remove(first)
+    if second in unconnected:
+        unconnected.remove(second)
+    return dist, circuits, first, second, unconnected
+    
+ex = True
+data = get_data()
 
 print("part 1 answer: %d" % part1(data))
-#print("part 2 answer: %d" % part2(data))
+print("part 2 answer: %d" % part2(data))
