537a0d872ab5df81a2de4b5f53a5102ffd76e840
james hedley <james.hedley@uk.ibm.com>
2025-12-08 20:27:06 +0000

    day 8 pt2 not working yet

--- 2025/day8.py
+++ 2025/day8.py
@@ -15,9 +15,8 @@ ex = True
 data = get_data()
 #print(data)
 
-def part1(data):
+def calc_distances(data):
     dist = {}
-
     for point in data:
         for pp in data:
             if point == pp:
@@ -25,8 +24,11 @@ def part1(data):
             if (pp,point) in dist.keys():
                 continue
             dist[(point, pp)] = math.sqrt((point[0]-pp[0])**2 + (point[1]-pp[1])**2 + (point[2]-pp[2])**2)
-
     dist = dict(sorted([(dx,dy) for dx,dy in dist.items()], key=lambda x: x[1]))
+    return dist
+
+def part1(data):
+    dist = calc_distances(data)
     circuits = []
     cables = 10 if ex else 1000
     for x in range(cables):
@@ -53,9 +55,43 @@ def part1(data):
                 break
         if not found:
             circuits.append([first,second])
-        
-    print(circuits)
-
     return math.prod([len(x) for x in sorted(circuits, key=lambda y: len(y), reverse=True)][:3])
 
+def part2(data):
+    dist = calc_distances(data)
+    circuits = []
+    cables = 0
+    while dist:
+    #while len(circuits) > 1 or cables < 10:
+        cables += 1
+        if cables % 10 == 0: print(cables)
+        #if len(dist) == 2:
+        #if len(circuits) == 1 and cables > 10:
+        if cables == 23:
+            print("break")
+        closest = list(dist.items())[0][0]
+        first, second = sorted(closest,key=lambda x: x[0])
+        del dist[closest]
+        found = [] # ((906, 360, 560), (984, 92, 344)) is in two circuits!
+        for cc in circuits:
+            oldcc = cc[:]
+            if first in cc and second in cc:
+                found.append(cc)
+            elif first in cc and not second in cc:
+                cc.append(second)
+                found.append(cc)
+            elif first not in cc and second in cc:
+                cc.append(first)
+                found.append(cc)
+            if len(found) > 1: #merge shared circuits
+                ncc = list(set(chain(*found)))
+                for fl in found:
+                    circuits.remove(fl)
+                circuits.append(ncc)
+                break
+        if not found:
+            circuits.append([first,second])
+    print(circuits)
+
 print("part 1 answer: %d" % part1(data))
+#print("part 2 answer: %d" % part2(data))
