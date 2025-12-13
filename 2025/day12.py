47a7f5ad577f88c884c6cd13d206cdd457353772
James Hedley <jameskhedley@gmail.com>
2025-12-13 17:21:05 +0000

    day12 complete

--- 2025/day12.py
+++ 2025/day12.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from itertools import chain
 
 def read_data():
     shapes = defaultdict(list)
@@ -15,8 +16,18 @@ def read_data():
         elif "x" in line:
             k = tuple([int(x) for x in line.split(":")[0].split("x")])
             v = [int(x) for x in line.split(":")[1][1:].split(" ")]
-            areas.append({k: v})
+            areas.append((k, v))
     return shapes, areas
 
-shapes, areas = read_data()
-pass
+def part1(): # wtf is this input?? super easy as it turns out
+    ans = 0
+    shapes, areas = read_data()
+    shape_sizes = dict((k, tuple(chain(*v)).count('#')) for k,v in shapes.items())
+    for area in areas:
+        size, counts = area
+        total_space_needed = [ cc * shape_sizes[idx] for idx, cc in enumerate(counts)]
+        if size[0]*size[1] > sum(total_space_needed):
+            ans+=1
+    return(ans)
+
+print("part 1 answer: %d" % part1())
